6f9a8096706cab0488eca3c7d3c048f9e01d023c
Virgile Carbonari <Virgile.Carbonari@mmt-b.com>
2025-07-02 09:10:41 +0200

    correct date fmt

--- hash.c
+++ hash.c
@@ -399,28 +399,49 @@ void do_bytes_rotation_1201(uint8_t* SI, uint8_t* DI) {
         if ((i + 1) % 8 == 0) printf("\n"); \
 	} \
 	printf("\n");
+
+void dosdatefmt(char dst[20], uint16_t time, uint16_t date){
+	static_assert(sizeof("16:01:00/16:06:2025") == 20);
+	dst[19] = 0;
+	sprintf(dst,
+		"%02d:%02d:%02d/%02d:%02d:%04d",
+		time >> 11 & 0x1f,
+		time >> 5 & 0x3f,
+		(time & 0x1f) * 2,
+		date & 0x1f,
+		date >> 5 & 0x0f,
+		(date >> 9 & 0x7f) + 1980
+	);
+}
 struct __attribute__((__packed__)) _user_entry {
 	char username[0x10];      // [0..0x0f]
 	uint8_t unknown_0[2];     // [0x10..0x11]
 	uint16_t unknown_word;    // [0x12] 0100:3780
-	uint16_t timestamp_cx;    // [0x14] 0100:37ad
-	uint16_t timestamp_dx;    // [0x16] 0100:37aa
+	uint16_t timestamp_time;    // [0x14] 0100:37ad
+	uint16_t timestamp_date;    // [0x16] 0100:37aa
 	uint8_t unknown[3];       // [0x17..0x19]
 	uint16_t syscall_0x10_dx; // [0x1a] 0100:3879
 	uint16_t syscall_0x10_cx; // [0x1c] 0100:3876
 };
+
 struct __attribute__((__packed__)) user_entry {
 	struct _user_entry entry;
 	uint8_t b[0x60-sizeof(struct _user_entry)];
 };
 void print_user_entry(struct user_entry *user){
+	char datebuff[20] = {0};	
+	dosdatefmt(
+		datebuff,
+		user->entry.timestamp_time,
+		user->entry.timestamp_date
+	);
 	printf(
 		"\"user\" : {\n"
 		"  \"name\": \"%s\",\n"
-		"  \"timestamp\": \"%02X:%02X\"\n"
+		"  \"timestamp\": \"%s\"\n"
 		"}\n",
 		(char*)user->entry.username,
-		user->entry.timestamp_dx, user->entry.timestamp_cx
+		datebuff
 	);
 }
 int main(int argc, char **argv){
